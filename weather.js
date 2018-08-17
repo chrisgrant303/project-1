bfff19311b7686680219b28704ebef34c585d52c
Luke Kennin <lkmeq@me.com>
2018-08-17 14:28:10 -0500

    finished weather/temp/time listener

--- weather.js
+++ weather.js
@@ -1,3 +1,6 @@
+var weather;
+var timeOfDay;
+var temperature;
 
 var weatherAPIKey = "cf49359bd70439b6a000e6a428e3e7fc";
 
@@ -5,16 +8,16 @@ var weatherURL = "https://api.openweathermap.org/data/2.5/weather?" +
     "q=Chicago,Illinois&units=imperial&appid=" + weatherAPIKey;
 
 var weatherID;
+var tempID;
 
 var timeDiv = $('#time');
-var time = moment().format("HH:mm");
+var time = moment().format("hh:mm");
 var hour = parseInt(moment().format("HH"));
 timeDiv.text("Time: " + time);
 
 $("#weather").on("click", function () {
     alert("Weather code: " + weatherID)
 });
-
 
 $.ajax({
     url: weatherURL,
@@ -24,63 +27,78 @@ $.ajax({
 
         var city = weatherResponse.name
 
-        console.log(weatherResponse);
-
         $(".city").html("<h1>" + weatherResponse.name + " Weather</h1>");
         $(".weather").html("Weather: " + weatherResponse.weather[0].description);
         $(".temp").html("Temperature (F) " + weatherResponse.main.temp);
 
-        console.log("City: " + weatherResponse.name);
-        console.log("Weather: " + weatherResponse.weather[0].description);
-        console.log("Temperature (F): " + weatherResponse.main.temp);
-
         //*IMPORTANT: BELOW IS THE ID (CODE/NUMER) THAT SPECIFIES THE WEATHER CONDITIONS. THIS WILL NOT BE DISPLAYED IN THE HTML*/
 
         weatherID = weatherResponse.weather[0].id;
-
-        console.log("WEATHER ID: " + weatherID)
+        tempID = weatherResponse.main.temp;
 
         if (weatherID === 800) {
-            console.log("it's clear outside")
+            weather = "clear";
         }
         else if (weatherID === 801 || weatherID === 802 || weatherID === 803) {
-            console.log("it's partly cloudy")
+            weather = "partly cloudy";
+        }
+        else if (weatherID === 804) {
+            weather = "cloudy";
         }
         else if (weatherID >= 700 && weatherID <= 781) {
-            console.log("it is misty outside")
+            weather = "misty";
         }
         else if (weatherID >= 600 && weatherID <= 621) {
-            console.log("it is snowing outside")
+            weather = "snowy";
         }
         else if (weatherID >= 500 && weatherID <= 531) {
-            console.log("it is raining outside")
+            weather = "raining";
         }
         else if (weatherID >= 300 && weatherID <= 321) {
-            console.log("it is drizzling outside")
+            weather = "drizzling";
         }
         else if (weatherID >= 200 && weatherID <= 232) {
-            console.log("it is heavily raining outside")
+            weather = "thunderstorms";
         }
         else {
-            console.log("error fetching weather data")
+            weather = "error";
         }
 
+        if (tempID >= 72 && tempID <= 80) {
+            temperature = "hot";
+
+        } else if (tempID >= 81 && tempID <= 110) {
+            temperature = "very hot";
+
+        } else if (tempID >= 58 && tempID <= 71) {
+            temperature = "mild";
+
+        } else if (tempID >= 48 && tempID <= 57) {
+            temperature = "cool"
+
+        } else if (tempID >= 0 && tempID <= 47) {
+            temperature = "cold"
+
+        }
+        if (hour >= 00 && hour < 06) {
+            timeOfDay = "early morning";
+        }
+        else if (hour >= 06 && hour < 12) {
+            timeOfDay = "morning";
+        }
+        else if (hour >= 12 && hour < 16) {
+            timeOfDay = "afternoon";
+        }
+        else if (hour >= 16 && hour < 19) {
+            timeOfDay = "evening";
+        }
+        else {
+            timeOfDay = "night";
+        }
+        console.log(timeOfDay);
+        console.log(weather);
+        console.log(temperature)
     });
-console.log(hour);
 
-if (hour >= 00 && hour < 06) {
 
-    console.log("early morning");
-}
-else if (hour >= 06 && hour < 12) {
-    console.log("morning");
-}
-else if (hour >= 12 && hour < 16) {
-    console.log("afternoon");
-}
-else if (hour >= 16 && hour < 19) {
-    console.log("evening");
-}
-else {
-    console.log("night");
-}
+
